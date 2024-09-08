5544af84aa8507a7e67d7d9010a620989607114e
Charles Gama <suelagama@gmail.com>
2024-09-08 16:03:49 -0300

    Make migrations a customize ModelAdmin

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from contact.models import Contact
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'id', 'first_name', 'last_name', 'phone', 'email'
+    ordering = '-id',
+    search_fields = 'first_name', 'last_name', 'phone', 'email'
+    list_per_page = 10
+    list_max_show_all = 100
+    list_display_links = 'first_name',
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField(max_length=254, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
