17f5fbfff799ce4a06628702bac8d32dd30493e1
Charles Gama <suelagama@gmail.com>
2024-09-10 14:43:01 -0300

    Create template and view single contact

--- contact/urls.py
+++ contact/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 from contact import views
-from django.conf.urls.static import static
-from django.conf import settings
+
+app_name = 'contact'
+
 
 urlpatterns = [
+    path('<int:contact_id>/', views.contact, name='contact'),
     path('', views.index, name='index'),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
